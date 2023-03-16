3791856c0322ec0982be640f39ea3cf538a86257
EkaterinaManuilova <e.manuilova@autosyst.ru>
2023-03-16 16:31:17 +0300

    fio head

--- form_ajax.js
+++ form_ajax.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $("form").submit(function (event) {
-        var formData = {
+        const formData = {
             user_name: $("#user_name").val(),
+            user_second_name: $("#user_second_name").val(),
             user_last_name: $("#user_last_name").val(),
         };
 
@@ -13,10 +14,13 @@ $(document).ready(function () {
             encode: true,
         }).done(function (result) {
             console.log(result);
-            for (let value of result) {
 
+            for (let value of result) {
+                console.log(value);
+                const fio_res = value.result.split(' ');
+                // console.log(value.result.split(' '));
                 $("#result").html(
-                    '<p>Полное имя ' + value.result + '</p><p>В лице ' + value.result_genitive + '</p><p>Кому ' + value.result_dative + '</p><p>Согласовано ' + value.result_ablative + '</p>'
+                    '<p>Фамилия ' + fio_res[0] + '</p><p>Имя ' + fio_res[1] + '</p><p> Отчество ' + fio_res[2] + '</p><p>Полное имя ' + value.result + '</p><p>В лице ' + value.result_genitive + '</p><p>Кому ' + value.result_dative + '</p><p>Согласовано ' + value.result_ablative + '</p>'
                 );
             }
 
